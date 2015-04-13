9e680e22c729ed4d9e51f3c458eba3e00291c6ec
liang <liang>
2015-04-13 14:42:09 +0800

    Translate, getWidth, getX in Rectangle
    box.translate(x,y)
    box.getWidth();
    box.getX();

--- draw/DrawACrate.java
+++ draw/DrawACrate.java
@@ -58,5 +58,10 @@ public class DrawACrate
 		redLine.setColor(Color.RED);
 		redLine.draw();
 		
+		//Translate, getWidth, getX in Rectangle
+		Rectangle box3=new Rectangle(80, 150, 20, 30);
+		box3.translate(25,25);
+		box3.draw();
+		System.out.println("box width is "+box3.getWidth()+" \nbox X coordinate is "+box3.getX());		
     }
 }
